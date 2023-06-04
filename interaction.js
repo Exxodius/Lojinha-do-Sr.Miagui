91f940fae981ef1da7f32a9013a1e25292d77a99
gabriel <gabrielexxonpletsch@gmail.com>
2023-06-04 12:32:44 -0300

    Itens_loja_1

---
+++ interaction.js
@@ -0,0 +1,19 @@
+let ITENS_LOJA = [
+    {
+        nome: "Bola da Copa",
+        preco: 86.30,
+        url_img: "./img/bola_copa.webp",
+        descricao: "Bola utilizada na Copa do Mundo, idela para utilizar em campo e colecionar"
+    },
+    {
+        nome: "Carrinho da Hot Wheels",
+        preco: 5.80,
+        url_img: "./img/carro_hotwheels.jpeg",
+        descricao: "Carrinho para dar de presentes para crianças ou colecionar"
+    },
+    {
+        nome: "Chinelo",
+        preco: 19.99,
+        url_img: "./img/chinelo.webp",
+        descricao: "Chinelo para se utilizar em qualquer lugar"
+    },
